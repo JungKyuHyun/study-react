eb787b82e520078c648409687eca75f7a434a7cb
jacob <ajdkfl6445@gmail.com>
2020-12-30 22:30:32 +0900

    Create tiny-react

---
+++ build/react.js
@@ -0,0 +1,67 @@
+const hooks = [];
+let currentComponent = -1;
+export class Component {
+  constructor(props) {
+    this.props = props;
+  }
+
+}
+export function useState(initValue) {
+  const position = currentComponent;
+
+  if (!hooks[position]) {
+    hooks[currentComponent] = initValue;
+  }
+
+  return [hooks[position], nextValue => {
+    hooks[position] = nextValue;
+  }];
+}
+
+function renderRealDOM(vdom) {
+  if (typeof vdom === 'string') {
+    return document.createTextNode(vdom);
+  }
+
+  if (vdom === undefined) {
+    return;
+  }
+
+  const $el = document.createElement(vdom.tagName);
+  vdom.children.map(renderRealDOM).forEach(node => {
+    $el.appendChild(node);
+  });
+  return $el;
+}
+
+export const render = function () {
+  let prevVdom = null;
+  return (nextVdom, container) => {
+    if (prevVdom !== nextVdom) {
+      prevVdom = nextVdom;
+    } // diff logic
+    // ...
+
+
+    container.appendChild(renderRealDOM(nextVdom));
+  };
+}();
+export function createElement(tagName, props, ...children) {
+  if (typeof tagName === 'function') {
+    if (tagName.prototype instanceof Component) {
+      const instance = new tagName({ ...props,
+        children
+      });
+      return instance.render();
+    } else {
+      currentComponent++;
+      return tagName.apply(null, [props, ...children]);
+    }
+  }
+
+  return {
+    tagName,
+    props,
+    children
+  };
+}
---
+++ src/index.js
@@ -0,0 +1,24 @@
+/* @jsx createElement */
+import { createElement, render, Component } from './react.js';
+
+console.log('jacob')
+
+class YourTitle extends Component {
+  render() {
+    return (
+      <p>나는 타이틀이 되고 싶은 당신</p>
+    )
+  }
+}
+
+function Title() {
+  return (
+    <div>
+      <h2>정말 동작 할까?</h2>
+      <YourTitle />
+      <p>잘 동작하는지 보고 싶다</p>
+    </div>
+  )
+}
+
+render(<Title />, document.querySelector('#root'));
